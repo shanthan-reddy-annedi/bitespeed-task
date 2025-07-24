18afa480a9e2040a4cb5d45829811ee242a69c4f
Shanthan Reddy <76593616+shanthan-reddy-annedi@users.noreply.github.com>
2025-07-24 16:02:08 +0530

    Updated the path of docs from api to docs

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
   console.log(
